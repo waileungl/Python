5c43009d2501a23db404cba2ee283f685ee5d897
William Liu <110699040+waileungl@users.noreply.github.com>
2022-10-06 09:05:25 -0700

    10/6

--- flask_mysql/crud/Users_CR/users.py
+++ flask_mysql/crud/Users_CR/users.py
@@ -1,29 +1,29 @@
 # import the function that will return an instance of a connection
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
-class Friend:
+class Users:
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
         self.last_name = data['last_name']
-        self.occupation = data['occupation']
+        self.occupation = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 # Now we use class methods to query our database
     @classmethod
     def get_all(cls):
-        query = "SELECT * FROM friends;"
+        query = "SELECT * FROM users;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
-        results = connectToMySQL('first_flask').query_db(query)
-        # Create an empty list to append our instances of friends
-        friends = []
+        results = connectToMySQL('users').query_db(query)
+        print("This is the result of 'SELECT * FROM users'", results)
+        users = []
         # Iterate over the db results and create instances of friends with cls.
-        for friend in results:
-            friends.append( cls(friend) )
-        return friends
+        for name in results:
+            users.append( cls(name) )
+        return users
 
     @classmethod
     def save(cls, data):
-        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        query = "INSERT INTO users ( first_name , last_name , email , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(email)s , NOW() , NOW() );"
         # data is a dictionary that will be passed into the save method from server.py
-        return connectToMySQL('first_flask').query_db( query, data)
+        return connectToMySQL('users').query_db( query, data)
--- flask_mysql/crud/first_flask_mysql/server.py
+++ flask_mysql/crud/first_flask_mysql/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
@@ -8,6 +8,20 @@ def index():
     # call the get all classmethod to get all friends
     print(myFriends)
     return render_template("index.html", myFriends = myFriends)
-            
+
+@app.route('/create_friend', methods=["POST"])
+def create_friend():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "occ" : request.form["occ"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Friend.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/')
+
 if __name__ == "__main__":
     app.run(debug=True)
