d2338677a82caa0c1add42d60fce7185764839ec
William Liu <110699040+waileungl@users.noreply.github.com>
2022-10-07 23:55:44 -0700

    10/7

--- flask_mysql/crud/Friendships/flask_app/controllers/users_control.py
+++ flask_mysql/crud/Friendships/flask_app/controllers/users_control.py
@@ -1,5 +1,6 @@
+from multiprocessing import Condition
 from flask_app import app
-from flask import render_template,redirect,request
+from flask import render_template,redirect,request,flash
 from flask_app.models.users import Users
 
 @app.route("/")
@@ -12,6 +13,8 @@ def friendships():
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
+    if len(request.form["last_name"]) == 0 or len(request.form["first_name"]) == 0:
+        return redirect("/friendships")
     data = {
         "last_name": request.form["last_name"],
         "first_name": request.form["first_name"]
@@ -21,9 +24,24 @@ def add_user():
 
 @app.route("/create_friendship", methods=["POST"])
 def create_friendship():
+
+    friendships = eval(request.form["friendships"])
+    for i in friendships:
+        Condition0 =  str(request.form["friends"]) == str(request.form["users"])
+        Condition1 = str(i["user_id"]) == str(request.form["friends"])
+        Condition2 = str(i["friend_id"]) == str(request.form["users"])
+        Condition3 = str(i["user_id"]) == str(request.form["users"])
+        Condition4 = str(i["friend_id"]) == str(request.form["friends"])
+        if (Condition1 and Condition3) or Condition0:
+            flash("You can't friend youself!")  
+            return redirect("/friendships")
+        elif (Condition1 and Condition2) or (Condition3 and Condition4):
+            flash("They are already friend!")  
+            return redirect("/friendships")
+
     data = {
         "user_id": request.form["users"],
-        "friend_id": request.form["friends"]
+        "friend_id": request.form["friends"],
     }
     Users.create_friendship(data)
     return redirect("/friendships")
